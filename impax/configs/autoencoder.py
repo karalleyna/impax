333d2729f4f5c5faad9673cc2f1a9cc70810e88c
karalleyna <aleynakara98@gmail.com>
2023-01-24 21:53:46 +0100

    Add configs of autoencoder and rgb2q

--- impax/configs/autoencoder.py
+++ impax/configs/autoencoder.py
@@ -1,254 +1,264 @@
-autoencoder_hparams = tf.contrib.training.HParams(
+"""
+Consists of the definition of the hyperparameters of a model.
+"""
+import ml_collections
+import optax
+
+
+def get_config():
+    config = ml_collections.ConfigDict()
     # [ob]: Whether to optimize the blobs.
-    ob='t',
+    config.optimize_blobs = "t"
     # [cp]: The constant prediction mode.
     # 'a': abs
     # 's': sigmoid.
-    cp='a',
+    config.prediction_mode = "a"
     # [dbg]: Whether to run in debug mode (checks for NaNs/Infs).
-    dbg='f',
+    config.debug_mode = False
     # [bs]: The batch size.
-    bs=24,  # 8,
+    config.batch_size = 24  # 8,
     # [h]: The height of the input observation.
-    h=137,
+    config.input_height = 137
     # [w]: The width of the input observation.
-    w=137,
+    config.input_width = 137
     # [lr]: The learning rate.
-    lr=5e-5,
+    config.learning_rate = 5e-5
     # Whether to 'nerfify' the occnet inputs:
-    hyo='f',
+    config.nerfify_occnet = False
     # Whether to 'nerfify' the pointnet inputs:
-    hyp='f',
+    config.nerify_pointnet = False
     # [loss]: A string encoding the loss to apply. Concatenate to apply several.
     #   'u': The uniform sample loss from the paper.
     #   'ns': The near surface sample loss from the paper.
     #   'ec': The element center loss from the paper.
-    loss='unsbbgi',
+    config.loss = "unsbbgi"
     # [lrf]: The loss receptive field.
     #   'g': Global: the loss is based on the global reconstruction.
     #   'l': Local: The loss is based on the local reconstruction.
     #   'x': Local Points: The loss is local points locally reconstructed.
-    lrf='g',
+    config.loss_receptive_field = "g"
     # [arch]: A string encoding the model architecture to use:
     #  'efcnn': A simple early-fusion feed-forward CNN.
     #  'ttcnn': A two-tower early-fusion feed-forward CNN. One tower for
     #           explicit parameters, and one tower for implicit parameters.
-    arch='efcnn',
+    config.model_architecture = "efcnn"
     # [cnna]: The network architecture of the CNN, if applicable.
     #  'cnn': A simple cnn with 5 conv layers and 5 FC layers.
     #  'r18': ResNet18.
     #  'r50': ResNet50.
     #  'h50': Tf-hub ImageNet pretrained ResNet50
-    cnna='r50',
+    config.cnn_architecture = "r50"
     # [da]: The type of data augmentation to apply. Not compatible with
     # arch=efcnn, because depth maps can't be augmented.
     # 'f': No data augmentation.
     # 'p': Panning rotations.
     # 'r': Random SO(3) rotations.
     # 't': Random transformations with centering, rotations, and scaling.
-    da='f',
+    config.data_augmentation = "f"
     # [cri]: Whether to crop the region in the input
-    cri='f',
+    config.crop_input = False
     # [crl]: Whether to crop the region in the loss
-    crl='f',
+    config.crop_loss = False
     # [cic]: The input crop count.
-    cic=1024,
+    config.num_input_crops = 1024
     # [clc]: The supervision crop count.
-    clc=1024,
+    config.num_supervision_crops = 1024
     # [ia]: The architecture for implicit parameter prediction.
     #  '1': Predict both implicit and explicit parameters with the same network.
     #  '2': Predict implicit and explicit parameters with two separate networks.
     #  'p': Predict implicit parameters with local pointnets.
-    ia='p',
+    config.implicit_architecture = "p"
     # [p64]: Whether to enable the pointnet 64-d transformation.
-    p64='f',
+    config.to_64d = False
     # [fua]: Whether to enable 3D rotation of the model latents.
-    fua='t',
+    config.apply_3d_rotation = True
     # [ipe]: Whether to enable implicit parameters. 't' or 'f'.
-    ipe='f',
+    config.enable_implicit_parameters = False
     # [ip]: The type of implicit parameters to use.
     #  'sb': Predict a minivector associated with each shape element that is
     #        residual to the prediction ('strictly better').
-    ip='sb',
+    config.implicit_parameters = "sb"
     # [ipc]: Whether the implicit parameters actually contribute (for debugging)
     # 't': Yes
     # 'f': No.
-    ipc='t',
+    config.debug_implicit_parameters = True
     # [ips]: The size of the implicit parameter vector.
-    ips=32,
+    config.implicit_parameter_length = 32
     # [npe]: Whether there is a separate network per shape element.
     #  't': Each shape element has its own neural network.
     #  'f': The shape elements share a single network.
-    npe='f',
+    config.seperate_network = False
     # [pe]: The point encoder to use.
     # 'pn': PointNet
     # 'dg': DG-CNN
-    pe='pn',
+    config.point_encoder = "pn"
     # [nf]: The DG-CNN feature count.
-    nf=64,
+    config.feature_count = 64
     # [fbp]: Whether to fix the very slow pointnet reduce.
-    fbp='t',
+    config.fix_pointnet = True
     # [mfc]: The maxpool feature count for PointNet.
-    mfc=1024,
+    config.maxpool_feature = 1024
     # [udp]: Whether to use the deprecated PointNet with Conv 2Ds.
-    udp='t',
+    config.use_pointnet = True
     # [orc]: The number of ResNet layers in OccNet
-    orc=1,
+    config.num_resnet_layers = 1
     # [sc]: The number of shape elements.
-    sc=100,
+    config.num_shape_elements = 100
     # [lyr]: The number of blobs with left-right symmetry.
-    lyr=10,
+    config.num_blobs = 10
     # [opt]: The optimizer to use.
-    #   'adm': Adam.
+    #   'adm': optax.Adam.
     #   'sgd': SGD.
-    opt='adm',
+    config.optimizer = optax.adam
     # [tx]: How to transform to element coordinate frames when generating
     # OccNet sample points.
     #   'crs': Center, rotate, scale. Any subset of these is fine, but they
     #     will be applied as SRC so 'cs' probably doesn't make much sense.
     #   'i': Identity (i.e. global coordinates).
-    tx='crs',
+    config.transformations = "crs"
     # [lhdn]: Whether to learn the sigmoid normalization parameter.
-    lhdn='f',
+    config.learn_sigmoid_normalization = False
     # [nrm]: The type of batch normalization to apply:
     #   'none': Do not apply any batch normalization.
     #   'bn': Enables batch norm, sets trainable to true, and sets is_training
     #     to true only for the train task.
-    nrm='none',
+    config.batch_normalization = None
     # [samp]: The input sampling scheme for the model:
     #   'imd': A dodecahedron of depth images.
     #   'imdpn': A dodecahedron of depth images and a point cloud with normals.
     #   'im1d': A single depth image from the dodecahedron.
     #   'rgb': A single rgb image from a random position.
     #   'imrd': One or more depth images from the dodecahedron.
-    samp='imdpn',
+    config.sampling_scheme = "imdpn"
     # [bg]: The background color for rgb images:
     # 'b': Black.
     # 'ws': White-smooth.
-    bg='ws',
+    config.background = "ws"
     # [lpc]: The number of local points per frame.
-    lpc=1024,
+    config.num_local_points = 1024
     # [rc]: The random image count, if random image samples are used.
-    rc=1,
+    config.num_random_images = 1
     # [spc]: The sample point count. If a sparse loss sampling strategy is
     #   selected, then this is the number of random points to sample.
-    spc=1024,
+    config.num_sample_points = 1024
     # [xsc]: The lrf='x' pre-sample point count. The number of samples taken
     # from the larger set before again subsampling to [spc] samples.
-    xsc=10000,
+    config.num_subsampled_points = 10000
     # [sync]: 't' or 'f'. Whether to synchronize the GPUs during training, which
     #   increases the effective batch size and avoid stale gradients at the
     #   expense of decreased performance.
-    sync='f',
+    config.synchronize_gpus = False
     # [gpu]: If 'sync' is true, this should be set to the number of GPUs used in
     #   training; otherwise it is ignored.
-    gpuc=0,
+    config.num_gpus = 0
     # [vbs]: The virtual batch size; only used if 'sync' is true. The number of
     #   training examples to pool before applying a gradient.
-    vbs=64,
+    config.virtual_batch_size = 64
     # [r]: 'iso' for isotropic, 'aa' for anisotropic and axis-aligned to the
     #   normalized mesh coordinates, 'cov' for general Gaussian RBFs.
-    r='cov',
+    config.coordinates = "cov"
     # [ir]: When refining a prediction with gradient descent, which points to
     #   use. Still needs to be refactored with the rest of the eval code.
-    ir='zero-set',
+    config.refine_prediction = "zero-set"
     # [res]: The rescaling between input and training SDFs.
-    res=1.0,
+    config.rescaling = 1.0
     # [pou]: Whether the representation is a Partition of Unity. Either 't' or
     #   'f'. If 't', the sum is normalized by the sum of the weights. If 'f',
     #   it is not.
-    pou='f',
+    config.is_partition = False
     # [didx]: When only one of the dodecahedron images is shown to the network,
     #   the index of the image to show.
-    didx=1,
+    config.index_of_dodecahedron = 1
     # [gh]: The height of the gaps (depth and luminance) images.
-    gh=224,
+    config.gap_height = 224
     # [gw]: The width of the gaps (depth and luminance) images.
-    gw=224,
+    config.gap_width = 224
     # [ucf]: The upweighting factor for interior points relative to exterior
     #   points.
-    ucf=1.0,
+    config.upweighting_factor = 1.0
     # [lset]: The isolevel of F defined to be the surface.
-    lset=-0.07,
+    config.isolevel = -0.07
     # [igt]: The inside-the-grid threshold in the element center lowres grid
     #   inside loss.
-    igt=0.044,  # Based on a 32^3 voxel grid with an extent of [-0.7, 0.7]
+    # Based on a 32^3 voxel grid with an extent of [-0.7, 0.7]
+    config.grid_threshold = 0.044
     # [wm]: Waymo scaling:
-    wm='f',
+    config.waymo_scaling = False
     # [rsl]: The rescaling factor for the dataset.
-    rsl=1.0,
-    # [ig]: The weight on the element center lowres grid inside loss.
-    ig=1.0,
-    # [gs]: The weight on the element center lowres grid squared loss.
-    gs=0.0,
+    config.rescaling = 1.0
+    # [ig]: The weight on the element center lowers grid inside loss.
+    config.inside_loss_weight = 1.0
+    # [gs]: The weight on the element center lowers grid squared loss.
+    config.squared_loss_weight = 0.0
     # [gd]: The weight on the element center lowres grid direct loss.
-    gd=0.0,
+    config.direct_loss_weight = 0.0
     # [cm]: The weight on the loss that says that element centers should have
     #   a small l2 norm.
-    cm=0.01,
+    config.l2_norm_weight = 0.01
     # [cc]: The weight on the component of the center loss that says that
     #   element centers must be inside the predicted surface.
-    cc=0.0,
+    config.element_center_weight = 0.0
     # [vt]: The threshold for variance in the element center variance loss.
-    vt=0.0,
+    config.element_center_variance_threshold = 0.0
     # [nnt]: The threshold for nearest neighbors in the element center nn loss.
-    nnt=0.0,
+    config.nearest_neighbors_threshold = 0.0
     # [vw]: The weight on the center variance loss
-    vw=0.0,
+    config.center_variance_loss_weight = 0.0
     # [nw]: The weight on the center nearest neighbor loss.
-    nw=0.0,
+    config.nearest_neighbor_loss_weight = 0.0
     # [ow]: The weight on the overlap loss:
-    ow=0.0,
+    config.overlap_loss_weight = 0.0
     # [dd]: Whether to turn on the deprecated single-shape occnet decoder.
     #   't' or 'f'.
-    dd='f',
+    config.use_occnet_decoder = False
     # [fon]: Whether to fix the occnet pre-cbn residual bug. 't' or 'f'.
-    fon='t',
+    config.fix_occnet = True
     # [itc]: The input pipeline thread count.
-    itc=12,
+    config.num_threads = 12
     # [dmn]: The amount of noise in the depth map(s).
-    dmn=0.0,
+    config.depth_map_noise = 0.0
     # [pcn]: The amount of noise in the point cloud (along the normals).
-    pcn=0.0,
+    config.point_cloud_noise = 0.0
     # [xin]: The amount of noise in the xyz image (in any direction).
-    xin=0.0,
+    config.xyz_noise = 0.0
     # [hdn]: The 'hardness' of the soft classification transfer function. The
     #   larger the value, the more like a true 0/1 class label the transferred
     #   F values will be, but the shorter the distance until the gradient is
     #   within roundoff error.
-    hdn=100.0,
+    config.hardness = 100.0
     # [ag]: The blob aggregation method. 's' for sum, 'm' for max.
-    ag='s',
+    config.aggregation_method = "s"
     # [l2w]: The weight on the 'Uniform Sample Loss' from the paper.
-    l2w=1.0,
+    config.uniform_sample_loss_weight = 1.0
     # [a2w]: The weight on the 'Near Surface Sample Loss' from the paper.
-    a2w=0.1,
+    config.near_surface_sample_loss_weight = 0.1
     # [fcc]: The number of fully connected layers after the first embedding
     #   vector, including the final linear layer.
-    fcc=3,
+    config.num_layers_before_final_layer = 3
     # [fcs]: The width of the fully connected layers that are immediately before
     #   the linear layer.
-    fcs=2048,
+    config.num_layers_before_linear = 2048
     # [ibblw]: The weight on the component of the center loss that says that
     #   element centers must be inside the ground truth shape bounding box.
-    ibblw=10.0,
+    config.center_loss_weight = 10.0
     # [aucf]: Whether to apply the ucf hyperparameter to the near-surface sample
     #  loss. 't' or 'f'.
-    aucf='f',
+    config.upweight = False
     # [cd]: Whether to cache the input data once read. 't' or 'f'.
-    cd='f',
+    config.cache = False
     # [elr]: The relative encoder learning rate (relative to the main LR)
-    elr=1.0,
+    config.relative_encoder_learning_rate = 1.0
     # [cat]: The class to train on.
-    cat='all',
-    # A unique identifying string for this hparam dictionary
-    ident='sif',
-    # Whether to balance the categories. Requires a batch size multiplier of 13
-    blc='t',
+    config.class_category = "all"
+    # [ident] : A unique identifying string for this hparam dictionary
+    config.hyperparams = "sif"
+    # [blc]: Whether to balance the categories. Requires a batch size multiplier of 13
+    config.balanced = True
+
     # [rec]: The reconstruction equations
-    rec='r',
-    # The version of the implicit embedding CNN architecture.
-    iec='v2'
-    
+    config.reconstruction_equation = "r"
+
+    # [iec]: The version of the implicit embedding CNN architecture.
+    config.implicit_embedding_version = "v2"
 
     return config
--- impax/configs/rgb2q.py
+++ impax/configs/rgb2q.py
@@ -1,44 +1,44 @@
 """
-Consists of the definition of the hyperparameters of a model.
+Consists of the definition of the hyperparameters of RGB2Q.
 """
 import ml_collections
 
 
 def get_config():
     config = ml_collections.ConfigDict()
-    # The task identifier, for looking up backwards compatible hparams (below):
+    # [identity]: The task identifier, for looking up backwards compatible hparams (below):
     # It is called identity in the original repo.
     config.id = "rgb2q"
-    # The batch size.
+    # [bs]: The batch size.
     # It is called bs in the original repo.
     config.batch_size = 32
-    # The learning rate.
+    # [lr]: The learning rate.
     # It is called lr in the original repo.
     config.learning_rate = 5e-05
-    # The number of primitives (must match the dataset).
+    # [sc]: The number of primitives (must match the dataset).
     # It is called sc in the original repo.
     config.num_primitives = 25
-    # The length of each primitive.
+    # [sd]: The length of each primitive.
     # It is called sd in the original repo.
     config.primitive_length = 42
-    # The length of the explicit component.
+    # [ed]: The length of the explicit component.
     # It is called ed in the original repo.
     config.explicit_component_length = 10
-    # The number of latent fully connected layers. >= 0.
+    # [fcc]: The number of latent fully connected layers. >= 0.
     # It is called fcc in the original repo.
     config.num_latent_fully_connected_layers = 2
-    # The width of the latent fully connected layers.
+    # [fcl]: The width of the latent fully connected layers.
     # It is called fcl in the original repo.
     config.latent_fully_connected_layer_width = 2
-    # ??
+    # [ft]: ??
     config.ft = True
-    # How to apply input augmentation: 'f', 't', 'io'
+    # [aug]: How to apply input augmentation: 'f', 't', 'io'
     config.input_augmentation = "f"
-    # The encoder architecture. 'rn', 'sr50', 'vl', 'vlp':
+    # [enc]: The encoder architecture. 'rn', 'sr50', 'vl', 'vlp':
     config.encoder = "rn"
-    # The levelset of the reconstruction:
+    # [lset]: The levelset of the reconstruction:
     config.level_set = -0.07
-    # The distillation loss type: 'l1', 'l2', or'hu'
+    # [l]: The distillation loss type: 'l1', 'l2', or'hu'
     config.distillation_loss = "l1"
     # [w]: The distillation loss weighting:
     # 'relative': Relative based on [ilw].
@@ -58,12 +58,12 @@ def get_config():
     #  'h50': Tf-hub ImageNet pretrained ResNet50
     config.cnn_architecture = "r50"
     # [rc]: The random image count, if random image samples are used.
-    config.random_image_count = 1
+    config.num_random_images = 1
 
     # [bg]: The background
     # 'b' or 'w' (black/white)
     # with or without 's' for smooth:
-    config._background = "ws"
+    config.background = "ws"
 
     # [dlw]: The distillation loss weight:
     config.distillation_loss_weight = (1.0,)
@@ -125,39 +125,40 @@ def get_config():
     # True for cam('c'  in the original repo)
     config.cam = True
     # [nlw]: The normal prediction loss weight.
-    config.normal_loss_weight = (1.0,)
+    config.normal_loss_weight = 1.0
     # [ni] Whether to add the normals as a secondary input.
     config.add_secondary_input = False
     # [nmo]: Whether to *only* input the normals:
-    nmo = False
+    config.is_normal_input = False
     # [dxw]: locals xyz loss weight
-    dxw = (1.0,)
+    config.xyz_loss_weight = 1.0
     # [dnw]: locals normal loss weight
-    dnw = (1.0,)
+    config.normal_loss_weight = 1.0
     # [drl]: The type of depth regression loss:
-    config.depth_regression_loss_type = ("l2",)
+    config.depth_regression_loss_type = "l2"
     # [msg]: What kind of segmentation to do in the depth -> XYZ mapping:
     # 'd': Do the mapping based on where the predicted depth is nontrivial.
     # 'p': Do the mapping based on the predicted segmentation mask.
     # 'g': Do the mapping based on the ground truth segmentation mask.
-    msg = ("p",)
+    config.depth_segmentation = "p"
     # [lpe]: Whether to have a local pointnet encoder. Only works if an xyz
     # image is available at test time:
-    lpe = ("f",)
+    config.local_pointnet_encoder = False
     # [lpn]: Whether to add normals as a feature for the local pointnets.
-    lpn = ("f",)
+    config.add_normals = False
     # [lpt]: The threshold (in radii) of the local pointclouds.
-    lpt = (4.0,)
+    config.threshold = (4.0,)
     # [vf]: Whether to add a point validity one-hot feature to the pointnet.
-    vf = ("f",)
+    config.point_validity = False
     # [fod]: Whether to fix the object_detection encoder defaults.
-    fod = ("t",)
+    config.fix_object_detection = True
     # [ftd]: Whether to finetune the decoder
-    ftd = ("f",)
+    config.finetune_decoder = False
     # # [iz]: Whether to ignore 0s in the local pointnet encoders.
     # iz='f',
-    lyr = (0,)
+    config.ignore_zeros = False
     # Whether to include global scope in the final feature vector.
-    grf = ("f",)
+    config.include_global_scope = False
     # The task identifier, for looking up backwards compatible hparams (below):
     config.task = "rgb2q"
+    return config
